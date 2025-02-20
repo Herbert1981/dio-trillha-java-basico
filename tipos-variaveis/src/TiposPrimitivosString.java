787526ee584f52d11edff2cda13e78006a9b314c
Herbert1981 <herbertlacerda@hotmail.com>
2025-02-20 16:50:14 -0300

    exercitando  tipos primitivos

---
+++ tipos-variaveis/src/TiposPrimitivosString.java
@@ -0,0 +1,29 @@
+public class TiposPrimitivosString {
+    
+    public static void main(String[] args) {
+
+        // Tipos  Primitivos
+        int idade = 43;
+        double altura = 1.74;
+        String inicial = "H";
+        boolean ehEstudante = true;
+
+        // Classe String
+        String nome = "Herbert Lacerda";
+
+        //Imprimindo informações
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("É Estudante? " + ehEstudante);
+
+
+
+       
+    }
+
+
+
+
+}
